1e79a444f939983fb67ddab353d64d7ff0b5972c
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-09-28 22:45:09 +0800

    v0.1.0

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use serde::Deserialize;
+use std::{ env, fs,
+    process::{ Command, Stdio },
+};
+
+#[derive(Deserialize, Debug)]
+struct Instruction {
+    source: String,
+}
+
+fn main() {
+    let inst_dir = {
+        let mut bin_d = env::current_exe()
+            .expect("Couldn't find the running binary.");
+        bin_d.pop();
+        bin_d.push("inst.toml");
+        bin_d
+    };
+
+    let inst: Instruction = toml::from_str(
+        fs::read_to_string(inst_dir)
+            .expect("failed to find `inst.toml`")
+            .as_str()
+    ).expect("failed to parse `inst.toml`");
+
+    let mut source = inst.source.split_whitespace();
+    let root = source.next()
+        .expect("no file specified in `inst.toml`");
+
+    Command::new(root)
+        .args(source)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect(format!("failed to execute command `{}`", &inst.source).as_str());
+}
