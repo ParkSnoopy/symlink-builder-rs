99261a2cce3feda22d13b83aff9a0794262cacf8
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-02-24 03:39:30 +0800

    update: multiple symlink in one directory
    Now the executable use own filename to read inst file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
-use std::{ env, fs,
-    process::{ Command, Stdio },
+use std::{
+    env,
+    fs,
+    process::{Command, Stdio},
 };
 
 #[derive(Deserialize, Debug)]
@@ -11,23 +13,20 @@ struct Instruction {
 fn main() {
     let user_args: Vec<String> = env::args().skip(1).collect();
 
-    let inst_dir = {
-        let mut bin_d = env::current_exe()
-            .expect("Couldn't find the running binary.");
-        bin_d.pop();
-        bin_d.push("inst.toml");
-        bin_d
-    };
+    let inst_file = env::current_exe()
+        .expect("Couldn't find the running binary.")
+        .as_path()
+        .with_extension("toml");
 
     let inst: Instruction = toml::from_str(
-        fs::read_to_string(inst_dir)
-            .expect("failed to find `inst.toml`")
-            .as_str()
-    ).expect("failed to parse `inst.toml`");
+        fs::read_to_string(inst_file)
+            .expect("Failed to find instruction file")
+            .as_str(),
+    )
+    .expect("Failed to parse instruction file");
 
     let mut source = inst.source.split_whitespace();
-    let root = source.next()
-        .expect("no file specified in `inst.toml`");
+    let root = source.next().expect("Bad instruction file format");
 
     let mut args: Vec<String> = source.map(|x| x.to_string()).collect();
     args.extend(user_args);
